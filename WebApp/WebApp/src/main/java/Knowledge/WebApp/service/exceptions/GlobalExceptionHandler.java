14ca7a8f4eeb01d5cdc35940bd987754ae111f12
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2024-12-22 03:40:14 +0200

    Implement GlobalExceptionHandler to handle application exceptions

---
+++ WebApp/WebApp/src/main/java/Knowledge/WebApp/service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package Knowledge.WebApp.service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return new ResponseEntity<>("Something went wrong: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
