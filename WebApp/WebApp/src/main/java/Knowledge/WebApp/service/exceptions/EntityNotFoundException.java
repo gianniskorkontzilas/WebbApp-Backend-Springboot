1b809a803b4764aeb8d4e962988fbda2beb65a5b
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2024-12-22 03:19:08 +0200

    Add EntityNotFoundException for handling missing entities

---
+++ WebApp/WebApp/src/main/java/Knowledge/WebApp/service/exceptions/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package Knowledge.WebApp.service.exceptions;
+
+public class EntityNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public EntityNotFoundException(Class<?> entityClass, Long id) {
+        super("Entity " + entityClass.getSimpleName() + " with id " + id + " was not found.");
+    }
+}
