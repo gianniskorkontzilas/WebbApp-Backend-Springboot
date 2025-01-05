749a45ec76ed121ff632ef8329dcb5ea0f2924a7
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-01-05 05:01:51 +0200

    Create User Entity

---
+++ WebApp/WebApp/src/main/java/Knowledge/WebApp/model/User.java
@@ -0,0 +1,20 @@
+package Knowledge.WebApp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+}
+
