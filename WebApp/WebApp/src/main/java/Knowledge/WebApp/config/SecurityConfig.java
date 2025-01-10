7b3a6f7cfc61ff431155505afa57576f7ce62388
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-01-11 01:40:44 +0200

    Add initial users with SHA-256 hashed passwords to the database (#8)

--- WebApp/WebApp/src/main/java/Knowledge/WebApp/config/SecurityConfig.java
+++ WebApp/WebApp/src/main/java/Knowledge/WebApp/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -35,7 +35,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())                .authorizeHttpRequests(authorize -> authorize
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -53,9 +53,15 @@ public class SecurityConfig {
         return authProvider;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new MessageDigestPasswordEncoder("SHA-256");
-    }
+
+@Bean
+public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
+
+
+
+
 }
 
