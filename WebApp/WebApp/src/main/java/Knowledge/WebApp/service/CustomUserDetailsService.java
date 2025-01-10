835bae604d964d71c62f3df172f8d50538f819b6
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-01-10 04:43:54 +0200

    Updated CustomUserDetailsService to load users from database #5

--- WebApp/WebApp/src/main/java/Knowledge/WebApp/service/CustomUserDetailsService.java
+++ WebApp/WebApp/src/main/java/Knowledge/WebApp/service/CustomUserDetailsService.java
@@ -26,7 +26,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 user.getLogin(),
                 user.getPassword(),
-                Collections.emptyList() // Authorities (e.g., roles)
+                Collections.emptyList() 
         );
     }
 }
