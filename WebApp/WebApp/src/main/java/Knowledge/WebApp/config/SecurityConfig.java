bc0ad3fd920b79d153865c71df32c2aea268ca69
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2025-01-20 02:54:27 +0200

    Configure CORS for frontend access

--- WebApp/WebApp/src/main/java/Knowledge/WebApp/config/SecurityConfig.java
+++ WebApp/WebApp/src/main/java/Knowledge/WebApp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import Knowledge.WebApp.security.Sha256PasswordEncoder;
 import Knowledge.WebApp.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,7 +15,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 
 @Configuration
 @EnableWebSecurity
@@ -35,12 +35,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorize -> authorize
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests(authorize -> authorize
                         .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT Filter
 
         return http.build();
     }
@@ -53,18 +55,8 @@ public class SecurityConfig {
         return authProvider;
     }
 
-
-//@Bean
-//public PasswordEncoder passwordEncoder() {
-//    return new BCryptPasswordEncoder();
-//}
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Sha256PasswordEncoder();
     }
-
-
-
 }
-
